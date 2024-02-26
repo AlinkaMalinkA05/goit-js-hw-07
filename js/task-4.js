fb49587dd9bcdf96f4b2fbae6109591bd125d13e
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-02-26 22:26:34 +0100

    4 is ready

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
-
+const sbmtForm = document.querySelector("form")
+sbmtForm.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+    const formEmail = this.elements.email.value;
+    const formPassword = this.elements.password.value;
+    if (formEmail === "" || formPassword === "") {
+        alert ("All form fields must be filled in")
+        
+    } else {
+        const newForm = {
+            email: formEmail,
+            password: formPassword
+        };
+        
+    this.reset() 
+    } 
+}
