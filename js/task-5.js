ea2797a15320ea755f3ee24319a300c6c213f24c
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-02-28 10:46:49 +0100

    5 is ready

--- js/task-5.js
+++ js/task-5.js
@@ -3,4 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+ const changedColor = getRandomHexColor;
 const btn = document.querySelector(".change-color");
+const bkgColor = document.querySelector (".color")
+btn.addEventListener("click", handleClick) 
+function handleClick() {
+  document.body.style.backgroundColor = changedColor;
+  bkgColor.textContent = changedColor;
+}
+ 
