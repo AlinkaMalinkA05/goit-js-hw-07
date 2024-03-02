44d091f5b90028d944fdb73eb2bff94882ef2129
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-03-02 16:15:22 +0100

    6 is changed

--- js/task-6.js
+++ js/task-6.js
@@ -7,11 +7,12 @@ function getRandomHexColor() {
 const input = document.querySelector('input');
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
-const boxesContainer = document.getElementById('boxes');
+const boxesContainer = document.querySelector('#boxes');
 
 createBtn.addEventListener('click', () => {
   const amount = Number(input.value);
   if (amount >= 1 && amount <= 100) {
+    boxesContainer.innerHTML = "";
     createBoxes(amount);
     input.value = '';
     input.classList.remove("error")
